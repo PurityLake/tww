16637f093788ca8a176cea8f57953cc42adf3a83
Robert O'Shea <robertoshea2k11@gmail.com>
2024-03-18 03:05:14 +0000

    Update daObjPlant_c members

--- include/d/actor/d_a_obj_plant.h
+++ include/d/actor/d_a_obj_plant.h
@@ -2,6 +2,7 @@
 #define D_A_OBJ_PLANT_H
 
 #include "d/d_bg_w.h"
+#include "d/d_cc_d.h"
 #include "d/d_com_inf_game.h"
 #include "f_op/f_op_actor.h"
 
@@ -20,6 +21,8 @@ public:
     /* Place member variables here */
     /* 0x290 */ request_of_phase_process_class mPhs;
     /* 0x298 */ J3DModel* mpModel;
+    /* 0x29C */ dCcD_Stts mStts;
+    /* 0x2D8 */ dCcD_Cyl mCyl;
 };
 
 #endif /* D_A_OBJ_PLANT_H */
